d2e9d52730896d8024ae26f673c9b6c84bf25f38
albert-tomanek <electron826@gmail.com>
2024-03-23 03:07:24 +0000

    Updates

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ import lxml.html as lh
 from lxml.etree import Element
 from typing import Iterator
 import requests
+import datetime
 
 class Website:
     def __init__(self) -> None:
@@ -20,16 +21,16 @@ class Website:
             self._groups = [c.text[1:] for c in e.xpath('//ol[@class="group-list"]/li/a')]
             return self._groups
     
-    def get_group(self, name):
+    def get_group(self, name=None):
         return GroupIter(name)
 
 class GroupIter:
-    def __init__(self, name) -> None:
+    def __init__(self, name=None) -> None:
         self.name = name
         self.last_id = None
 
         self.page = lh.fromstring(
-            requests.get(f'https://tildes.net/~{name}').text
+            requests.get(f'https://tildes.net/~{name}' if name else 'https://tildes.net/').text
         )
         self.post_iter: Iterator[Element] = iter(self.page.xpath('//*[@class="topic-listing"]//article'))
 
@@ -38,6 +39,7 @@ class GroupIter:
 
     def __next__(self):
         try:
+            print(self.name)
             art = next(self.post_iter)
         except StopIteration:
             self.next_page()
@@ -46,13 +48,18 @@ class GroupIter:
         assert art.attrib['id'].startswith('topic-')
         id = self.last_id = art.attrib['id'][6:]
 
-        return {
-            "title": art.xpath('.//*[@class="topic-title"]//a')[0].text,
-            "id": id,
-            "votes": art.xpath('.//*[@class="topic-voting-votes"]')[0].text,
-            "ctime": art.xpath('.//time')[0].attrib['datetime'],
-            "author": art.attrib['data-topic-posted-by']
-        }
+        item = PostItem(self)
+        item.title = art.xpath('.//*[@class="topic-title"]//a')[0].text
+        item.url = art.xpath('.//*[@class="topic-title"]//a')[0].attrib['href']
+        item.id = id
+        item.votes = art.xpath('.//*[@class="topic-voting-votes"]')[0].text
+        item.ctime = datetime.datetime.fromisoformat(art.xpath('.//time')[0].attrib['datetime'])
+        item.author = art.attrib['data-topic-posted-by']
+        item.ncomments = int(art.xpath('.//*[@class="topic-info-comments"]//a')[0].text_content().split()[0])
+        comments_link = art.xpath('.//*[@class="topic-info-comments"]//a')[0].attrib['href']
+        item.is_selfpost = (item.url == comments_link)
+
+        return item
     
     def next_page(self):
         self.page = lh.fromstring(
@@ -63,7 +70,23 @@ class GroupIter:
     @property
     def page_len(self) -> int:
         return len(self.page.xpath('//*[@class="topic-listing"]//article'))
-    
+
+class PostItem:
+    def __init__(self, grp) -> None:
+        self.grp = grp
+
+        self.title = None
+        self.url = None
+        self.id = None
+        self.votes = None
+        self.ctime = None
+        self.author = None
+        self.ncomments = None
+        self.is_selfpost = None
+
+    def get_post(self):
+        return Post(self.grp.name, self.id)
+
 class Post:
     def __init__(self, group, id) -> None:
         self.group = group
@@ -75,7 +98,17 @@ class Post:
 
     @property
     def text(self) -> str:
-        return self.page.xpath('//article/*[@class="topic-full-text"]')[0].text_content().strip()
+        try:
+            return self.page.xpath('//article/*[@class="topic-full-text"]')[0].text_content().strip()
+        except IndexError:
+            return None
+
+    @property
+    def url(self) -> str:
+        try:
+            return self.page.xpath('//article/*[@class="topic-full-link"]//a')[0].attrib['href']
+        except IndexError:
+            return None
 
     @property
     def title(self) -> str:
@@ -86,8 +119,8 @@ class Post:
         return self.page.xpath('//article/header//*[@class="link-user"]')[0].text
 
     @property
-    def ctime(self) -> str:
-        return self.page.xpath('//article/header//time')[0].attrib['datetime']
+    def ctime(self):
+        return datetime.datetime.fromisoformat(self.page.xpath('//article/header//time')[0].attrib['datetime'])
 
     @property
     def votes(self) -> int:
@@ -102,10 +135,10 @@ class Post:
     def comments(self):
         return [Comment(elt) for elt in self.page.xpath('//*[@id="comments"]/*/article')]
 
-class Comment:  # Wraps an `article` element representing a comment
+class Comment:
     def __init__(self, elt) -> None:
+        self.elt = elt  # The `article` HTML element that this comment wraps
         assert elt.tag == 'article'
-        self.elt = elt
 
     @property
     def text(self) -> str:
@@ -117,11 +150,19 @@ class Comment:  # Wraps an `article` element representing a comment
 
     @property
     def ctime(self) -> str:
-        return self.elt.xpath('./*[@class="comment-itself"]//time')[0].attrib['datetime']
+        return datetime.datetime.fromisoformat(self.elt.xpath('./*[@class="comment-itself"]//time')[0].attrib['datetime'])
 
     @property
     def votes(self) -> int:
         return int(self.elt.xpath('./*[@class="comment-itself"]//*[@class="comment-votes"]')[0].text.split()[0])  # `15 votes`
+    
+    @property
+    def is_op(self) -> bool:
+        return ('is-comment-by-op' in self.elt.attrib['class'].split())
+
+    @property
+    def is_exemplary(self) -> bool:
+        return ('is-comment-exemplary' in self.elt.attrib['class'].split())
 
     @property
     def replies(self):
